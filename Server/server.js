5915c1e61ca628b62cc677bb02f595093d508be5
James Hosken <jh3284@James.lan>
2016-04-11 10:15:00 +0400

    Port ENV and io.config

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 5000
+
+
+//Because Heroku doesn't like websockets?
+io.configure(function () {  
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
@@ -14,6 +23,6 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on' +  port);
 });
